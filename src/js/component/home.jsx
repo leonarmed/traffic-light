d49e835d952e70231e989f671e290f512de1c502
Leonardo Medina <leonarmed@gmail.com>
2022-08-25 00:26:35 +0000

    bugfixing setinterval

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,41 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [active, setActive] = useState('')
+	const [loop, setLoop] = useState(false)
+	let [position, setPosition] = useState(0) 
+	const lights = [{color:'red'},{color:'yellow'},{color:'green'}]
+
+	useEffect(()=>{
+		const color = ['red','yellow','green']
+		let ColorInterval = setInterval((prev)=>{
+			// setActive(color[position])
+			// setPosition[prev++]
+			if(position <= color.length-1){
+				console.log(color[position])
+				console.log(position)
+				setActive(color[position])
+				setPosition(position++)
+			}else{
+				console.log("es mayor")
+				console.log(position)
+				setPosition(0)
+			}
+		},1000)
+	},[loop])
+
+	
 	return (
 		<div className="container">
 			<div className="support-trafic-light"></div>
 			<div className="light-container">
-				<div className="light red my-2"></div>
-				<div className="light yellow my-2"></div>
-				<div className="light green my-2"></div>
+				{lights.map((light) => {
+					const {color} = light;
+					return <div onClick={()=>{setActive(light.color)}} className={`light ${color} my-2 ${active===color ? 'active' : ''}`}></div>	
+				})}
 			</div>
+			<button className="btn btn-primary mt-2" onClick={()=>{setLoop(!loop)}}>Activar por tiempo</button>
 		</div>
 	);
 };
